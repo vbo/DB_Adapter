769a8fc9b805107fcb71512c638e213cc3e2e1fb
Borodin Vadim <=>
2010-09-12 15:47:38 +0400

    test for DB_Adapter_Factory class

---
+++ test/DB/Adapter/FactoryTest.php
@@ -0,0 +1,82 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once 'DB/Adapter/Factory.php';
+
+class DB_Adapter_FactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider dsnProvider
+     */
+    public function testParseDSNCommon($dsn, $parsed)
+    {
+        $this->assertEquals($parsed, DB_Adapter_Factory::parseDSN($dsn));
+    }
+    
+    /**
+     * @dataProvider dsnProviderBad
+     */
+    public function testParseDSNBad($dsn)
+    {
+        // we must parse bad dsns silently.
+        // Error here - its DBMS problem
+        DB_Adapter_Factory::parseDSN($dsn);
+    }
+
+    public function dsnProviderBad()
+    {
+        return array(
+            array('i am bad'),
+            array('')
+        );
+    }
+
+    public function dsnProvider()
+    {
+        return array(
+            array( // common dsn
+                'dbtype://username:pass@dbhost/dbname?param=val',
+                array(
+                    'scheme' => 'dbtype',
+                    'host' => 'dbhost',
+                    'user' => 'username',
+                    'pass' => 'pass',
+                    'path' => '/dbname',
+                    'query' => 'param=val',
+                    'param' => 'val',
+                    'dsn' => 'dbtype://username:pass@dbhost/dbname?param=val',
+                )
+            ),
+            array( // dsn with @ simbol in password
+                'dbtype://username:@pas@s@dbhost/dbname?param=val',
+                array(
+                    'scheme' => 'dbtype',
+                    'host' => 'dbhost',
+                    'user' => 'username',
+                    'pass' => '@pas@s',
+                    'path' => '/dbname',
+                    'query' => 'param=val',
+                    'param' => 'val',
+                    'dsn' => 'dbtype://username:@pas@s@dbhost/dbname?param=val',
+                )
+            ),
+            array( // dsn with specified port
+                'dbtype://username:pass@dbhost:1234/dbname?param=val',
+                array(
+                    'scheme' => 'dbtype',
+                    'host' => 'dbhost',
+                    'user' => 'username',
+                    'port' => 1234,
+                    'pass' => 'pass',
+                    'path' => '/dbname',
+                    'query' => 'param=val',
+                    'param' => 'val',
+                    'dsn' => 'dbtype://username:pass@dbhost:1234/dbname?param=val',
+                )
+            ),
+            array( // already parsed dsn
+                array('bla'=>'bla'),
+                array('bla'=>'bla')
+            )
+        );
+    }
+}
