691bb2f593ed85235e368de1c34d2258640f49b5
Borodin Vadim <vb@in-source.ru>
2011-02-18 21:15:25 +0300

    short tags removed from guestbook tmpl

--- usage/guestbook/_template.php
+++ usage/guestbook/_template.php
@@ -1,19 +1,19 @@
-<h1>DB_Adapter usage example &rarr; guestbook (<?=get_class($DB)?>)</h1>
-<form method="post">
+<h1>DB_Adapter usage example &rarr; guestbook (<?php echo get_class($DB); ?>)</h1>
+<form method="post" action="">
     <p>
         <b>Автор</b><br />
         <input name="author" />
     </p>
     <p>
         <b>Текст*</b><br />
-        <textarea name="text"></textarea>
+        <textarea name="text" cols="" rows=""></textarea>
     </p>
     <input type="submit" />
 </form>
 <br />
 <?php foreach ($messages as $message):?>
     <p>
-        <?=$message['text']?><br />
-        <small><b><?=$message['author']?></b> <?=substr($message['created'], 0, 19)?></small>
+        <?php echo $message['text']?><br />
+        <small><b><?php echo $message['author']?></b> <?php echo substr($message['created'], 0, 19)?></small>
     </p>
 <?php endforeach;?>
