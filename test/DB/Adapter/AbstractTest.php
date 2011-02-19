c2e0c4f8d57302c4d7eac1c3b68f77af159c81a9
Borodin Vadim <vb@in-source.ru>
2011-02-20 00:59:39 +0300

    hardcoded ident prefix in abstract test

--- test/DB/Adapter/AbstractTest.php
+++ test/DB/Adapter/AbstractTest.php
@@ -21,7 +21,9 @@ abstract class DB_Adapter_AbstractTest extends PHPUnit_Framework_TestCase
             self::$_dsn = parse_ini_file(dirname(__FILE__) . '/../../../config/db-credentials.ini');
         }
         if (empty(self::$_connections[$this->_dbtype])) {
-            self::$_connections[$this->_dbtype] = DB_Adapter_Factory::connect(self::$_dsn[$this->_dbtype]);
+            $con = DB_Adapter_Factory::connect(self::$_dsn[$this->_dbtype]);
+            $con->setIdentPrefix('test_');
+            self::$_connections[$this->_dbtype] = $con;
         }
         return self::$_connections[$this->_dbtype];
     }
