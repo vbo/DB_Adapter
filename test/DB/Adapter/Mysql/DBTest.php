b400a40eae631090d413715266f9d7102237c11d
Borodin Vadim <vb@in-source.ru>
2011-02-20 01:51:47 +0300

    mysql dbtest improved

--- test/DB/Adapter/Mysql/DBTest.php
+++ test/DB/Adapter/Mysql/DBTest.php
@@ -6,10 +6,32 @@ class DB_Adapter_Mysql_DBTest extends DB_Adapter_Abstract_DBTest
 {
     protected $_dbtype = 'mysql';
 
+    /**
+     * Perform direct DB query
+     */
+    private function _query($query)
+    {
+        return @mysql_query($query, $this->_getDB()->getLink());
+    }
+
+    private function _selectCell($query)
+    {
+        return mysql_result($this->_query($query), 0);
+    }
+
+    public function testQueryPerforms()
+    {
+        $name = md5(time()) . 'db_adapter_unittest';
+        $setted = 'bla';
+        $this->_getDB()->query("SET @{$name} = '{$setted}'");
+        $getted = $this->_selectCell('SELECT @' . $name);
+        $this->assertEquals($getted, $setted);
+    }
+
     protected function _createTestTables()
     {
-        $this->_getDB()->query('
-            CREATE TABLE ?_user (
+        $this->_query('
+            CREATE TABLE test_user (
                 id int NOT NULL,
                 login varchar(100) NOT NULL,
                 mail varchar(400) NOT NULL,
@@ -21,6 +43,6 @@ class DB_Adapter_Mysql_DBTest extends DB_Adapter_Abstract_DBTest
 
     protected function _dropTestTables()
     {
-        $this->_getDB()->query('DROP TABLE ?_user');
+        $this->_query('DROP TABLE test_user');
     }
 }
